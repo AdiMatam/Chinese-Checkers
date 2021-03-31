45effc51dce26e15e980d58c1f794e1ddd73ac49
Aditya Matam <aditya0310@outlook.com>
2021-03-30 18:24:16 -0700

    Removed extraneous, commented code

--- src/helpers.cpp
+++ src/helpers.cpp
@@ -7,42 +7,9 @@ float STEP           = RADIUS * 2 + GAP;
 float BOARD_DIAMETER = RADIUS * 2 * 17 + GAP * 19;
 float HEIGHT         = BOARD_DIAMETER + 2 * GAP;
 
-// void logf (const char* format, ...) {
-//     va_list args;
-//     va_start(args, format);
-//     vfprintf(stdout, format, args);
-//     printf("\n");
-//     va_end(args);
-// }
-
 void logColor(const sf::Color& color) {
     printf("[%d, %d, %d]\n", static_cast<int>(color.r), static_cast<int>(color.g), static_cast<int>(color.b));
 }
-
-// void loadTexture(sf::Texture* texture, const std::string& file) {
-//     if (!texture->loadFromFile(file))
-//         throw std::exception();
-// }
-
-// void loadAllTextures(sf::Texture (&textureArray)[12]) {
-//     dirent* entry;
-//     DIR* dp;
-
-//     dp = opendir("img");
-//     if (dp == NULL) {
-//         perror("opendir: Path does not exist or could not be read.");
-//         return;
-//     }
-//     int x = 0;
-//     std::string name;
-//     while ((entry = readdir(dp))) {
-//         name = entry->d_name;
-//         if (name.size() < 3) continue;
-//         loadTexture(&textureArray[x], "img/" + name);
-//         x++;
-//     }
-//     closedir(dp);
-// }
 
 bool keyPressed(const sf::Event& e, const sf::Keyboard::Key& code) {
     return e.type == sf::Event::KeyPressed && e.key.code == code;
--- src/helpers.hpp
+++ src/helpers.hpp
@@ -11,13 +11,6 @@ extern float STEP          ;
 extern float RADIUS        ;
 extern float THICK         ; 
 
-// template <typename T>
-// void log (T object) {
-//     std::cout << object << std::endl;
-// }
-
-// void logf (const char* format, ...);
-
 void logColor(const sf::Color& color); 
 
 template <typename T>
@@ -25,17 +18,8 @@ void logVector(const sf::Vector2<T>& vec) {
     std::cout << "[" << vec.x << ", " << vec.y << "]\n";
 }
 
-// void loadTexture(sf::Texture* texture, const std::string& file);
-
-// void loadAllTextures(sf::Texture (&textureArray)[12]);
-
 bool keyPressed(const sf::Event& e, const sf::Keyboard::Key& code);
 bool mousePressed(const sf::Event& e, const sf::Mouse::Button& code);
 
-// template <int N>
-// void drawSpriteArray(sf::RenderWindow* window, sf::Sprite* sprites) {
-//     for (int i = 0; i < N; i++)
-//         window->draw(sprites[i]);
-// }
 
 #endif
